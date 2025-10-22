3ac206d6c4c3cb4e29c524b292b325e3e76cc2e6
Curtis McKinney <curtismckinney@gmail.com>
2025-10-21 20:02:35 -0400

    fix(signup): correct Signup.css import path

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
-import './Signup.css';
+import '../Signup.css';
 import useRedirectIfAuthed from './hooks/useRedirectIfAuthed';
 
 function Signup() {
